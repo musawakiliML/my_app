a1b012d0818c7444a17cf0bcdb7f8bdbd91a7416
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-11-09 13:24:29 +0100

    My application

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+      title: "My Flutter App",
+      home: Text("This is a test App", 
+    textDirection: TextDirection.ltr),
+    ) 
+  );
+}
