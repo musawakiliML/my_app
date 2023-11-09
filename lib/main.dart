c06b29429be9532d2a91e020484435896fef7f19
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-11-09 13:37:51 +0100

    Update: Added first screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    const MaterialApp(
-      title: "My Flutter App",
-      home: Text("This is a test App", 
-    textDirection: TextDirection.ltr),
-    ) 
-  );
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("I am Flutter Developer"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.lightBlue[300],
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://storage.googleapis.com/cms-storage-bucket/70760bf1e88b184bb1bc.png'),
+            ),
+        )
+      ),
+  ));
 }
